28f2aba9b7c1fbd70524af4928aa4d3d3e78f407
sakshikaleves <96903857+sakshikaleves@users.noreply.github.com>
2024-10-08 22:19:41 +0530

    Your commit message here

--- pages/client-dashboard/[clientID].js
+++ pages/client-dashboard/[clientID].js
@@ -405,6 +405,7 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { PieChart, Pie, Cell, Tooltip, Legend, BarChart, Bar, XAxis, YAxis, CartesianGrid } from 'recharts';
+import Sidebar from "../../components/dashboard/sidebar"
 
 const ClientDashboard = () => {
   const router = useRouter();
@@ -467,7 +468,7 @@ const ClientDashboard = () => {
   return (
     <div className="flex min-h-screen bg-gray-100">
       {/* Sidebar Navigation */}
-      <nav className="w-64 bg-blue-500 text-black p-5">
+      {/* <nav className="w-64 bg-blue-500 text-black p-5">
         <div className="text-xl font-bold mb-4">Dashboard</div>
         <ul className="space-y-3">
           <li onClick={() => router.push('/campaigns')}>Campaigns</li>
@@ -475,7 +476,9 @@ const ClientDashboard = () => {
           <li onClick={() => router.push('/settings')}>Settings</li>
           <li onClick={() => router.push('/login')}>Logout</li>
         </ul>
-      </nav>
+      </nav> */}
+
+      <Sidebar/>
 
       {/* Main Content */}
       <div className="flex-grow p-5">
